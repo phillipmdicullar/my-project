7726e0b85404f71760df7d6a94cbf3ab72b752ac
Philip <phillip.emdokolo@student.moringaschool.com>
2024-09-20 20:53:44 +0300

    navbar and routing completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Header() {
+    const menuItems = [
+        {
+            title:"Home",
+            key:"/"
+        },
+        {
+            title:"Projects",
+            key:"/projects"
+        },
+        {
+            title:"Contact",
+            key:"/contact"
+        },
+        {
+            title:"Courses",
+            key:"/courses"
+        }
+    ]
+  
   return (
-    <div><h1>Header Component</h1></div>
+    <div className='text-white font-mont'>
+      <div className="flex bg-theme justify-between items-center p-2 shadow-lg">
+        <div>
+         <h1 className='text-4xl font-semibold p-3'>coder<span className="blinking">_🤣</span></h1>
+        </div>
+        <div>
+           {/* {menuItems.map((item) => {
+            return <li key={item.key}>
+              <Link to={`./${item.key}`}>{item.title}</Link>
+            </li>
+           })} */}
+           <ul className='flex m-4 p-2 space-x-4 '>
+           {menuItems.map((item) => {
+                return (
+                  <li className='list-none p-2' key={item.key}>
+                    <Link to={item.key}>{item.title}</Link>
+                  </li>
+                );
+              })}
+           </ul>
+        </div>
+      </div> 
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        theme: '#2A2A2A'
+      },
+      fontFamily:{
+        mont : ['Montserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
